7e749db960cef361c91be13c10ed94063f812e21
Melissa <melissa.monnier@etu.hesge.ch>
2017-01-21 10:36:48 +0100

    An alien planet called Fungy where a strange creature lives...

---
+++ Assets/Dragon/CASetAnimBool.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CASetAnimBool : CustomActionScript {
+
+	public Animator _animatorTarget;
+	public string _animBoolName;
+	public bool _animBoolValue = true;
+
+
+	public override void OnDrawGizmos()
+	{
+		base.OnDrawGizmos();
+
+		if (_animatorTarget != null)
+			Debug.DrawLine(this.transform.position, _animatorTarget.transform.position, Color.blue);
+
+	}
+
+	public override IEnumerator DoActionOnEvent (MonoBehaviour sender, GameObject args)
+	{
+		yield return new WaitForFixedUpdate();
+		if (_animatorTarget != null) {
+			_animatorTarget.SetBool (_animBoolName, _animBoolValue); 
+		}
+		yield return null;
+	}
+
+}
---
+++ Assets/sound.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sound : MonoBehaviour {
+
+	public bool Alreadyplay;
+	public AudioSource soundSource;
+
+	// Use this for initialization
+	void Start () {
+		soundSource = GetComponent<AudioSource> ();
+		Alreadyplay = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider trigger) {
+		if (!Alreadyplay && trigger.gameObject.name =="FPSconrtoller") {
+			soundSource.Play();
+			Alreadyplay = true;
+		}
+	}
+}
